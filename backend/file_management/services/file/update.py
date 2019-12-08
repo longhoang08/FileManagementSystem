d3f7a358fe282a0671c34c28654b5b82137aded0
jian <thanhdatuetk62@gmail.com>
2019-12-08 22:19:44 +0700

    fix change size when move file

--- backend/file_management/services/file/update.py
+++ backend/file_management/services/file/update.py
@@ -1,5 +1,6 @@
 from . import es
 from config import FILES_INDEX
+from .utils import get_ancestors
 
 
 def update(file_id, **kwargs):
@@ -7,7 +8,7 @@ def update(file_id, **kwargs):
     fields = ['file_title', 'file_type', 'owner', 'star', 'parent_id', 'share_mode', 'editable', 'users_shared',
               'children_id', 'size', 'description', 'file_tag', 'created_at', 'updated_at']
     file = es.get_source(index=FILES_INDEX, id=file_id)
-    
+
     for field in fields:
         if field in kwargs:
             new_value = kwargs.get(field)
@@ -18,6 +19,9 @@ def update(file_id, **kwargs):
                 old_value.sort()
 
             if new_value != old_value:
+                if field == 'parent_id':
+                    update_size(old_value, new_value, file['size'])
+
                 update_body[field] = new_value
 
     es.indices.refresh(index=FILES_INDEX)
@@ -29,3 +33,56 @@ def update(file_id, **kwargs):
         }
     )
     return res
+
+
+def update_size(old_parent, new_parent, file_size):
+    """
+    This Method should not be called directly
+    :param old_parent: Old Parent
+    :param new_parent: New Parent
+    :param file_size: File Size
+    :return: Something amazing!
+    """
+    old_ancestors = set(get_ancestors(old_parent))
+    new_ancestors = set(get_ancestors(new_parent))
+
+    negotiate_size = list(old_ancestors.difference(new_ancestors))
+    advance_size = list(new_ancestors.difference(old_ancestors))
+
+    es.indices.refresh(index=FILES_INDEX)
+    es.update_by_query(
+        index=FILES_INDEX,
+        body={
+            "query": {
+                "terms": {
+                    "file_id": negotiate_size
+                }
+            },
+            "script": {
+                "lang": "painless",
+                "source": "ctx._source.size += params.capacity",
+                "params": {
+                    "capacity": -1 * file_size
+                }
+            }
+        }
+    )
+
+    es.indices.refresh(index=FILES_INDEX)
+    es.update_by_query(
+        index=FILES_INDEX,
+        body={
+            "query": {
+                "terms": {
+                    "file_id": advance_size
+                }
+            },
+            "script": {
+                "lang": "painless",
+                "source": "ctx._source.size += params.capacity",
+                "params": {
+                    "capacity": file_size
+                }
+            }
+        }
+    )
