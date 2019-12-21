11af117e3e59c01913d90fe2a37aa37a2e00992a
Long Hoang Bao <longhoang0811@gmail.com>
2019-12-21 22:16:21 +0700

    hot fix mail

--- backend/file_management/services/pending_register.py
+++ backend/file_management/services/pending_register.py
@@ -7,6 +7,7 @@ from file_management.extensions.custom_exception import RegisterBeforeException,
 from file_management.extensions.exceptions import BadRequestException
 from file_management.helpers import encode_token
 from file_management.helpers import validate_register, hash_password
+from file_management.services import mail_service
 from file_management.templates import email_template
 # from file_management.services import mail_service
 
@@ -53,5 +54,6 @@ def send_confirm_email(username, email, fullname, **kwargs):
     confirm_token = encode_token(email, int(os.environ['TOKEN_UPTIME']))
     active_link = os.environ['SERVER_BASE_URL'] + 'register/confirm_email/' + confirm_token
     msg_html = email_template.gen_confirm_email_body_template(fullname, username, active_link)
-    from file_management.celery.tasks import send_mail
-    send_mail("uFile's email confirmation", email, msg_html)
+    # from file_management.celery.tasks import send_mail
+    # send_mail("uFile's email confirmation", email, msg_html)
+    mail_service.send_email("uFile's email confirmation", email, msg_html)
