4a670d594cf93a1a355b62536db75efee5526004
Long Hoang Bao <longhoang0811@gmail.com>
2019-12-21 23:51:16 +0700

    remove test

--- backend/main.py
+++ backend/main.py
@@ -12,12 +12,5 @@ _logger = logging.getLogger(__name__)
 _DOT_ENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(_DOT_ENV_PATH)
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
-
 if __name__ == '__main__':
     app.run(debug=True)
