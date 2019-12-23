b2f7b12f0915e23a418ff8187cdb3716cddd3dab
Long Hoang Bao <longhoang0811@gmail.com>
2019-12-23 23:42:46 +0700

    notification api

--- backend/file_management/repositories/notification.py
+++ backend/file_management/repositories/notification.py
@@ -7,10 +7,26 @@ from file_management import models as m
 
 
 def get_all_notitfication(user_id):
-    print(user_id)
     return m.Notification.query.filter(
         m.Notification.user_id == user_id
     ).order_by(m.Notification.created_at).limit(10)
+
+
+def get_notifications_by_ids(ids, user_id):
+    return m.Notification.query.filter(
+        m.Notification.id.in_(ids)
+    ).filter(m.Notification.user_id == user_id).filter(m.Notification.viewed == False)
+
+
+def marked_as_readed(notifications):
+    updated = False
+    for notification in notifications:
+        notification.viewd = True
+        updated = True
+    m.db.session.commit()
+    return {
+        "updated": updated
+    }
 
 
 def add_new_notification(**kwargs):
